f2ea41c5e5d886f2add77ca53e452e139a599714
MathisPastreNelson <shiray@hotmail.fr>
2023-01-23 02:22:43 +0100

    ajout d'une videoyoutube en embed

--- src/components/VideoCarousel.js
+++ src/components/VideoCarousel.js
@@ -3,7 +3,15 @@ import React from 'react'
 export default function VideoCarousel() {
     return (
         <div>
-            VideoCarousel
+            <iframe
+                width="100%"
+                height="600"
+                src={"https://www.youtube.com/embed/8Ww47V7O4J0"}
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                title="Embedded youtube"
+            />
         </div>
     )
 }
